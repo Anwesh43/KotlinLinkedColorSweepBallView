9bc72195868865d500827031ee5461c6131cfee8
Anwesh43 <anweshthecool0@gmail.com>
2018-06-23 15:53:58 +0530

    adding CSVState class

--- linkedcolorsweepballview/src/main/java/com/anwesh/uiprojects/linkedcolorsweepballview/LinkedColorSweepView.kt
+++ linkedcolorsweepballview/src/main/java/com/anwesh/uiprojects/linkedcolorsweepballview/LinkedColorSweepView.kt
@@ -25,4 +25,26 @@ class LinkedColorSweepView(ctx : Context) : View(ctx) {
         }
         return true
     }
+
+    data class CSVState (var j : Int = 0, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        val scales : Array<Float> = arrayOf(0f, 0f)
+
+        fun update(stopcb : (Float) -> Unit) {
+            scales[j] += 0.1f * dir
+            if (Math.abs(scales[j] - prevScale) > 1) {
+                scales[j] = prevScale + dir
+                dir = 0f
+                prevScale = scales[j]
+                stopcb(prevScale)
+            }
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1 - 2 * prevScale
+                startcb()
+            }
+        }
+    }
 }
